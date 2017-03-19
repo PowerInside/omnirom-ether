0a6a733b4f6d95a3be06013ab5c4eb3d893ac452
Mohammed Irfan <irfan@hamon.in>
2017-03-19 11:37:49 +0530

    Prepare machine with tmux

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get install -qy curl
+sudo apt-get install -qy curl tmux
 sudo apt-get dist-upgrade -qy
 curl -fsSL https://get.docker.com/ | sh
 docker build -t omnirom-ether .
